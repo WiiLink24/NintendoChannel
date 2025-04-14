5d2320a96829dff20e427b0925181445f4bc7dd5
Sketch <75850871+SketchMaster2001@users.noreply.github.com>
2025-04-14 16:55:52 -0400

    feat: Allow for invoking selections via cmd args

--- cli/main.go
+++ cli/main.go
@@ -6,18 +6,35 @@ import (
 	"NintendoChannel/v6/dllist"
 	"NintendoChannel/v6/thumbnail"
 	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
+	var selection int
+
+	// Allow for arguments to be passed for autogenerate.
+	if len(os.Args) > 1 {
+		var err error
+		selection, err = strconv.Atoi(os.Args[1])
+		if err != nil {
+			log.Fatalln("Invalid selection")
+		}
+		selector(selection)
+	}
+
 	fmt.Println("WiiLink Nintendo Channel File Generator")
 	fmt.Println()
 	fmt.Println("1. DLList and game info\n2. DLList only\n3. Thumbnails\n4. CSData\n5. v3 List")
 	fmt.Println()
 	fmt.Printf("Choose: ")
 
-	var selection int
 	fmt.Scanln(&selection)
+	selector(selection)
+}
 
+func selector(selection int) {
 	switch selection {
 	case 1:
 		dllist.MakeDownloadList(true)
