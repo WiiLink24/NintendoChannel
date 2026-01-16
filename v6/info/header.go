5a232a923ca58570b09df1faa10114210965f2c3
Emmet <ebbswim1@gmail.com>
2026-01-15 21:09:11 -0500

    Added support for Wii Point values on certain VC platforms

--- v6/info/header.go
+++ v6/info/header.go
@@ -110,4 +110,20 @@ func (i *Info) MakeHeader(gameID [4]byte, numberOfPlayers uint8, companyID uint3
 	default:
 		i.Header.IsPurchasable = 0
 	}
+	switch titleType { // Can't guarantee the amount of shop points for anything but Virtual Console
+	case constants.NES:
+		i.Header.ShopPoints = 0x1F4
+	case constants.SNES:
+		i.Header.ShopPoints = 0x320
+	case constants.Nintendo64:
+		i.Header.ShopPoints = 0x3E8
+	case constants.Genesis:
+		i.Header.ShopPoints = 0x320
+	case constants.NeoGeo:
+		i.Header.ShopPoints = 0x384
+	case constants.MasterSystem:
+		i.Header.ShopPoints = 0x1F4
+	default:
+		i.Header.ShopPoints = 0xFFFFFFFF
+	} //TODO: implement the rest of the shop points for different regions (imports may be 100 or more points than usa) and publishers cuz inconsistency
 }
