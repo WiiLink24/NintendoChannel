4e34ffd1c1cf5c7e1783be9a0e1c48ae88ed497e
Emmet <ebbswim1@gmail.com>
2026-01-27 01:08:28 -0500

    fix: video type on "Popular Videos" now displayed

--- v6/dllist/videos.go
+++ v6/dllist/videos.go
@@ -38,7 +38,7 @@ type PopularVideosTable struct {
 	ID          uint32
 	VideoLength uint16
 	TitleID     uint32
-	BarColor    uint8
+	VideoType   uint8
 	_           [15]byte
 	RatingID    uint8
 	Unknown     uint8
@@ -155,7 +155,7 @@ func (l *List) MakePopularVideoTable() {
 			ID:          uint32(id),
 			VideoLength: uint16(length),
 			TitleID:     0,
-			BarColor:    0,
+			VideoType:   uint8(videoType),
 			RatingID:    9,
 			Unknown:     1,
 			VideoRank:   1,
