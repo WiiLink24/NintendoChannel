186a52108a91afb3d49ac5d193d799517463c89a
Emmet <ebbswim1@gmail.com>
2026-02-14 14:44:09 -0500

    chore: scan "videoType" as uint8

--- v6/dllist/videos.go
+++ v6/dllist/videos.go
@@ -63,7 +63,7 @@ func (l *List) MakeVideoTable() {
 		var id int
 		var queriedTitle string
 		var length int
-		var videoType int
+		var videoType uint8
 		var dateAdded time.Time
 
 		err = rows.Scan(&id, &queriedTitle, &length, &videoType, &dateAdded)
@@ -123,7 +123,7 @@ func (l *List) MakeNewVideoTable() {
 		var id int
 		var queriedTitle string
 		var length int
-		var videoType int
+		var videoType uint8
 
 		err = rows.Scan(&id, &queriedTitle, &length, &videoType, nil)
 		common.CheckError(err)
@@ -172,7 +172,7 @@ func (l *List) MakePopularVideoTable() {
 		var id int
 		var queriedTitle string
 		var length int
-		var videoType int
+		var videoType uint8
 
 		err = rows.Scan(&id, &queriedTitle, &length, &videoType, nil)
 		common.CheckError(err)
